8b471de9112800bebdebc6bba73cfad9ec9b2e29
DaliVidic <daliborka_vg@hotmail.com>
2025-01-04 21:23:36 +0000

    Last functions - Park service

--- park-service/index.ts
+++ park-service/index.ts
@@ -2,45 +2,83 @@ import {
     RaccoonMeadowsVolunteers,
     RaccoonMeadowsActivity,
     raccoonMeadowsVolunteers,
-  } from './raccoon-meadows-log';
-  
-  import {
+} from "./raccoon-meadows-log";
+
+import {
     WolfPointVolunteers,
     WolfPointActivity,
     wolfPointVolunteers,
-  } from './wolf-point-log';
-  
-  type CombinedActivity = RaccoonMeadowsActivity | WolfPointActivity;
-  
-  type Volunteers = {
+} from "./wolf-point-log";
+
+type CombinedActivity = RaccoonMeadowsActivity | WolfPointActivity;
+
+type Volunteers = {
     id: number;
     name: string;
     activities: CombinedActivity[];
-  };
-  
-  function combineVolunteers(
+};
+
+function combineVolunteers(
     volunteers: (RaccoonMeadowsVolunteers | WolfPointVolunteers)[]
-  ) {
-  
-  }
-  
-  function calculateHours(volunteers: Volunteers[]) {
+) {
     return volunteers.map((volunteer) => {
-      let hours = 0;
-  
-      volunteer.activities.forEach((activity) => {
-  
-      });
-  
-      return {
-        id: volunteer.id,
-        name: volunteer.name,
-        hours: hours,
-      };
+        let id = volunteer.id;
+        if (typeof id === "string") {
+            id = parseInt(id, 10);
+        }
+        return {
+            id: id,
+            name: volunteer.name,
+            activities: volunteer.activities,
+        };
     });
-  }
-  
-  const combinedVolunteers = combineVolunteers(
+}
+function byHours(
+    a: { id: number, name: string, hours: number },
+    b: { id: number, name: string, hours: number }
+) {
+    return b.hours - a.hours
+}
+
+function getHours(activity: CombinedActivity) {
+    return 'hours' in activity ? activity.hours : activity.time;
+}
+
+function calculateHours(volunteers: Volunteers[]) {
+    return volunteers.map((volunteer) => {
+        let hours = 0;
+        volunteer.activities.forEach((activity) => {
+            if (isVerified(activity.verified)) {
+                hours += getHours(activity);
+
+            }
+        });
+
+        return {
+            id: volunteer.id,
+            name: volunteer.name,
+            hours: hours,
+        };
+    });
+
+}
+function isVerified(verified: string | boolean) {
+    if (typeof verified === 'string') {
+        if (verified === 'Yes') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    return verified;
+}
+
+const combinedVolunteers = combineVolunteers(
     [].concat(wolfPointVolunteers, raccoonMeadowsVolunteers)
-  );
-  
+);
+
+//console.log(combinedVolunteers);
+
+const result = calculateHours(combinedVolunteers);
+
+console.log(result.sort(byHours));
